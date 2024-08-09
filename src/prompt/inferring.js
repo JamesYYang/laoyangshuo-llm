db5ea07f2b6aae35c4b634b0aa322284b8431332
James Yang <james.yang516@outlook.com>
2024-08-09 19:33:12 +0800

    add ai cs

--- src/prompt/inferring.js
+++ src/prompt/inferring.js
@@ -11,6 +11,20 @@ let lamp_review = `
   在我看来，Lumina 是一家非常关心顾客和产品的优秀公司！
   `
 
+let story = `
+在疫情冲击和经济低迷的双重打击下，最近两年任何企业，任何个人其实日子都不好过。我们看到了很多曾经风光的大厂都开启了大规模的裁员。
+不知道你思考过没，企业选择裁员的逻辑是什么？裁员到底有没有用？
+
+我们可以类比一下减肥。正常的减肥思维，肯定是哪里肉多减哪里对吧？
+你会不会说我们要平均分配一下，身体每个部分都减一下，又不是神经病。
+
+所以，同理可得，裁员的本质是要去除冗余。就好像减肥一样，减肥的目的是要让自己更健康，所以，要把身体里不好的冗余去除。
+企业也是一样，伴随着裁员，企业在过程中应该是去识别冗余，剔除那些对企业可有可无的业务，可有可无的员工。
+当然，要做到这一点看似很容易，其实对于高管（VP以上）要求是挺高的，至少高管心里对于公司的整体营运情况是非常清晰的，包括每个业务投入多少资源？
+产了多大的收益？这些问题要随时能答的上来，否则怎么知道哪里有冗余？
+难道就靠算一下人力成本？难道就看一下每个人的工作负荷？会不会太low了……
+`
+
 let emotionAnalysis = async () => {
 
   const promptTemplate = PromptTemplate.fromTemplate(
@@ -54,10 +68,35 @@ let productReviewAnalysis = async () => {
   console.log(res)
 }
 
+let subjectAnalysis = async ()=>{
+  const promptTemplate = PromptTemplate.fromTemplate(
+    `
+确定以下给定文本中讨论的五个主题。
+
+每个主题用1-2个词概括。
+
+请输出一个可解析的Json数组，每个元素是一个字符串，展示了一个主题。
+
+给定文本: \`\`\`{story}\`\`\`
+    `)
+
+  let output = await promptTemplate.invoke({ story: story })
+
+  let llm = new ChatBaiduQianfan({ modelName: 'ERNIE-Bot-4' })
+
+  let parser = new JsonOutputParser()
+
+  let res = await llm.pipe(parser).invoke(output)
+
+  console.log(res)
+}
+
 let doJob = async () => {
   // await emotionAnalysis()
 
-  await productReviewAnalysis()
+  // await productReviewAnalysis()
+
+  await subjectAnalysis()
 }
 
 
